3cf4b002da76c7b3f6dab84a82a878711b486300
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-07-17 23:25:29 +0930

    Create translate functions

---
+++ javascript/translate.js
@@ -0,0 +1,37 @@
+import { morseDict, englishDict } from "./dict.js";
+
+function translateToMorse(str) {
+    let result = "";
+    for (const char of str) {
+        const key = char.toUpperCase();
+        if (Object.keys(morseDict).includes(key)) {
+            result += morseDict[key] + " ";
+        }
+    }
+    return result;
+}
+
+function translateToEnglish(str) {
+    let input = str.split(" ");
+    let result = "";
+    for (const char of input) {
+        if (Object.keys(englishDict).includes(char)) {
+            result += englishDict[char];
+        }
+    }
+    return result;
+}
+
+function isMorse(input) {
+    const morseChars = ["-", ".", "/", " "];
+    return input.split("").every((char) => morseChars.includes(char));
+}
+
+export function translate(input) {
+    let toTranslate = input.trim();
+    if (toTranslate.length > 0 && !isMorse(toTranslate)) {
+        return translateToMorse(toTranslate);
+    } else if (toTranslate.length > 0) {
+        return translateToEnglish(toTranslate);
+    }
+}
